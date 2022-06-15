278c24cf6dba41b1b4f7bcfc01f69a53e2f1117a
Davyd Zakorchennyi <Diza41a@mail.com>
2022-06-15 14:33:01 -0400

    Getting ready for mvp deployment

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-const { MONGODB_URI } = require('../config');
+// const { MONGODB_URI } = require('../config');
+// process.env.MONGODB_URI = MONGODB_URI;
 
-process.env.MONGODB_URI = MONGODB_URI;
+process.env.MONGODB_URI = 'mongodb://ec2-18-208-151-10.compute-1.amazonaws.com:27017/reviewsAPI';
 const dbConnection = mongoose.connect(process.env.MONGODB_URI, {
 });
 
--- mongodb_mock.js
+++ mongodb_mock.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 {
   "product": {
     type: Number,
