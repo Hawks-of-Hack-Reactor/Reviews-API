1a5b2b97563a508f7073c7832361f4e0e4d39cb7
Davyd Zakorchennyi <69057079+Diza41a@users.noreply.github.com>
2022-06-16 19:14:00 -0400

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // const { MONGODB_URI } = require('../config');
 // process.env.MONGODB_URI = MONGODB_URI;
 
-process.env.MONGODB_URI = 'mongodb://ec2-18-233-98-61.compute-1.amazonaws.com:27017/';
+process.env.MONGODB_URI = 'mongodb://ec2-52-207-128-14.compute-1.amazonaws.com:27017/reviewsAPI';
 const dbConnection = mongoose.connect(process.env.MONGODB_URI, {
 });
 
